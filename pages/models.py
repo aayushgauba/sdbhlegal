df759db4db4be921e731f5730a9bf0eace7ea99a
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2024-09-04 21:07:44 -0500

    Admin Portal

--- pages/models.py
+++ pages/models.py
@@ -4,3 +4,6 @@ class Contact(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
+
+    def __str__(self):
+        return self.name
