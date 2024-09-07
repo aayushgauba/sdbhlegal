4898e6f0c15785c6cf7943e9d375c9b22d84e062
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2024-09-07 17:25:18 -0500

    Bug Fixes

--- send_emails.py
+++ send_emails.py
@@ -1,20 +1,28 @@
 # send_emails.py
 import time
-from pages.models import Contact
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 import django
 import os
+import sys
+import os
+
 
 # Set up Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sdbhlegal.settings')
 django.setup()
+# Add the directory containing the 'pages' module to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from pages.models import Contact
+
+
 
 def send_email_task():
     contacts = Contact.objects.filter(mailSent=False)
     for contact in contacts:
         html_content = render_to_string('userConfirmation.html')
-        
+
         email = EmailMultiAlternatives(
             subject=f"Thank you for contacting SDBH Legal",
             body=contact.message,
