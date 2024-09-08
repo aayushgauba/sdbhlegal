b5b2169f9c8b64a36dd1a37b6a933c83a1f8968b
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2024-09-07 22:53:52 -0500

    Index now

--- pages/views.py
+++ pages/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Contact
+from indexnow import IndexNow
 
 def index(request):
+    updated_url = "https://www.sdbhlegal.com"
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow.index([updated_url])
     return render(request, "index.html")
 
 def about(request):
+    updated_url = "https://www.sdbhlegal.com/about"
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow.index([updated_url])
     return render(request, "about.html")
 
 def expertise(request):
+    updated_url = "https://www.sdbhlegal.com/expertise"
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow.index([updated_url])
     return render(request, "expertise.html")
 
 def choose(request):
+    updated_url = "https://www.sdbhlegal.com/choose"
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow.index([updated_url])
     return render(request, "choose.html")
 
 def sitemap(request):
     return HttpResponse(open('sitemap.xml').read(), content_type='text/xml')
 
 def mediation(request):
+    updated_url = "https://www.sdbhlegal.com/mediation"
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow.index([updated_url])    
     return render(request, "mediation.html")
 
 def contact(request):
+    updated_url = "https://www.sdbhlegal.com/contact"
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow.index([updated_url])     
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
