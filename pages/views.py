7a7e047111bf9ebd89cd5e96c9302b00d81d04da
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2024-09-15 12:03:08 -0500

    Index now fix

--- pages/views.py
+++ pages/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Contact
 from indexnow import IndexNow
+import requests
 
 def index(request):
     updated_url = "https://www.sdbhlegal.com"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
     indexnow.index([updated_url])
     return render(request, "index.html")
 
 def about(request):
     updated_url = "https://www.sdbhlegal.com/about"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
     indexnow.index([updated_url])
     return render(request, "about.html")
 
 def expertise(request):
     updated_url = "https://www.sdbhlegal.com/expertise"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
     indexnow.index([updated_url])
     return render(request, "expertise.html")
 
 def choose(request):
     updated_url = "https://www.sdbhlegal.com/choose"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
     indexnow.index([updated_url])
     return render(request, "choose.html")
 
@@ -31,14 +32,14 @@ def sitemap(request):
 
 def mediation(request):
     updated_url = "https://www.sdbhlegal.com/mediation"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
     indexnow.index([updated_url])    
     return render(request, "mediation.html")
 
 def contact(request):
     updated_url = "https://www.sdbhlegal.com/contact"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="https://www.sdbhlegal.com")
-    indexnow.index([updated_url])     
+    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
+    status = indexnow.index([updated_url])
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
