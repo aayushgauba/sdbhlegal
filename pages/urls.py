f541b0f6c5cb1e81561fbc7aac21cb8216c55bb6
Aayush Gauba <55630226+aayushgauba@users.noreply.github.com>
2024-09-21 14:42:56 -0500

    Bug Fixes

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', views.index, name='index'),  # Home page
@@ -11,4 +12,5 @@ urlpatterns = [
     path('mediation', views.mediation, name='mediation'),
     path('contact', views.contact, name='contact'),
     path('sitemap', views.sitemap, name='sitemap'),
+    path('76dfebe7687d4416a7d5cd0eda143fc8.txt', serve, {'document_root': settings.STATICFILES_DIRS[0], 'path': '7fcc06c5e3184e13ab70cae01c1e6126.txt'}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pages/views.py
+++ pages/views.py
@@ -1,31 +1,43 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Contact
 from indexnow import IndexNow
-import requests
 import os
+
+indexnow = IndexNow(key="76dfebe7687d4416a7d5cd0eda143fc8", host="sdbhlegal.com")
+urls_to_index = []
+
+def submit_urls():
+    global urls_to_index
+    if urls_to_index:
+        try:
+            indexnow.index(urls_to_index)
+        except Exception as e:
+            # Silencing the error
+            print(f"An error occurred: {e}")
+        urls_to_index = []
 
 def index(request):
     updated_url = "https://www.sdbhlegal.com"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
-    indexnow.index([updated_url])
+    urls_to_index.append(updated_url)
+    submit_urls()
     return render(request, "index.html")
 
 def about(request):
     updated_url = "https://www.sdbhlegal.com/about"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
-    indexnow.index([updated_url])
+    urls_to_index.append(updated_url)
+    submit_urls()
     return render(request, "about.html")
-
+    
 def expertise(request):
     updated_url = "https://www.sdbhlegal.com/expertise"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
-    indexnow.index([updated_url])
+    urls_to_index.append(updated_url)
+    submit_urls()
     return render(request, "expertise.html")
 
 def choose(request):
     updated_url = "https://www.sdbhlegal.com/choose"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
-    indexnow.index([updated_url])
+    urls_to_index.append(updated_url)
+    submit_urls()
     return render(request, "choose.html")
 
 def sitemap(request):
@@ -36,19 +48,19 @@ def sitemap(request):
 
 def mediation(request):
     updated_url = "https://www.sdbhlegal.com/mediation"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
-    indexnow.index([updated_url])    
+    urls_to_index.append(updated_url)
+    submit_urls()
     return render(request, "mediation.html")
 
 def contact(request):
     updated_url = "https://www.sdbhlegal.com/contact"
-    indexnow = IndexNow(key="7f3322dd1da64affa340bcb94eabc61a", host="sdbhlegal.com")
-    status = indexnow.index([updated_url])
+    urls_to_index.append(updated_url)
+    submit_urls()
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
         message = request.POST.get("message")
-        contact = Contact(name=name, email=email, message=message, mailSent = False)
+        contact = Contact(name=name, email=email, message=message, mailSent=False)
         contact.save()
         return redirect('contact')
     return render(request, "contact.html")
